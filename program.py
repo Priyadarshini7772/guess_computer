e63bba2b674ca17e0d4768481857ff0cf09bc4ba
Priyadarshini7772 <161844172+Priyadarshini7772@users.noreply.github.com>
2024-03-29 14:54:48 +0530

    Update program.py

--- program.py
+++ program.py
@@ -1,17 +1,17 @@
 import random
-def guess(x):
+def guess(max):
     fb=''
-    y=1
+    min=1
     while fb!='c':
 
-        guess=random.randint(y,x)
+        guess=random.randint(min,max)
         fb=input(f'Is {guess} correct? (l)low or (h)high or (c)correct : ')
         if fb=='l':
             print('sorry try again. Too low ')
-            y=guess+1
+            min=guess+1
         elif fb=='h':
             print('sorry try again. Too high')
-            x=guess-1
-    print('Yay! computer found correct answer!!')
-x=int(input('Give range:'))
+            max=guess-1
+    print('Yay! computer found your number!!')
+max=int(input('Give range:'))
 guess(x)
